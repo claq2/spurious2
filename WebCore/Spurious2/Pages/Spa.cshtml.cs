8c11ee58ec53f3ac59de2aad3cdb060fd665683f
James McLachlan <JMcLachlan@carbonite.com>
2024-01-30 13:50:50 -0500

    Add sec headers nuget

---
+++ WebCore/Spurious2/Pages/Spa.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Spurious2.Pages
+{
+    public class SpaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
