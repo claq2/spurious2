3e6c3c238f353ebda68eb0e4becb1ffedb76b61b
James McLachlan <JMcLachlan@carbonite.com>
2024-02-05 19:02:26 -0500

    Update about, remove test code

--- WebCore/Spurious2/Pages/Spa.cshtml.cs
+++ WebCore/Spurious2/Pages/Spa.cshtml.cs
@@ -1,65 +1,11 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using NetTopologySuite.IO.Converters;
-using Spurious2.Core;
-using Spurious2.Infrastructure.Models;
-using System.Text.Json;
 
 namespace Spurious2.Pages
 {
-    public class SpaModel(SpuriousContext context, ISpuriousRepository spuriousRepository) : PageModel
+    public class SpaModel() : PageModel
     {
-        static readonly JsonSerializerOptions jsonOptions;
-        static SpaModel()
+        public void OnGet()
         {
-            jsonOptions = new JsonSerializerOptions { ReadCommentHandling = JsonCommentHandling.Skip };
-            jsonOptions.Converters.Add(new GeoJsonConverterFactory());
-        }
-
-        public async Task OnGet()
-        {
-            //var subDivs = context.Subdivisions
-            //    .Where(s => s.Id == 1001101)
-            //    //.Select(s => s.GeographicCentre.AsText())
-            //    .ToList();
-
-            //using (var memStream = new MemoryStream())
-            //{
-            //    using (var writer = new Utf8JsonWriter(memStream))
-            //    {
-            //        JsonSerializer.Serialize(writer, subDivs[0].GeographicCentre, jsonOptions);
-            //    }
-
-            //    var pointJson = Encoding.UTF8.GetString(memStream.ToArray());
-            //}
-
-            //using (var memStream = new MemoryStream())
-            //{
-            //    using (var writer = new Utf8JsonWriter(memStream))
-            //    {
-            //        JsonSerializer.Serialize(writer, subDivs[0].Boundary, jsonOptions);
-            //    }
-
-            //    var boundaryJson = Encoding.UTF8.GetString(memStream.ToArray());
-            //}
-
-            //var subDivs2 = context.Subdivisions
-            //    .Where(s => s.Id == 1001101)
-            //    .Select(s => s.Boundary.AsText())
-            //    .ToList();
-
-            //var stores = context.Stores
-            //    .Where(s => s.Id == 1)
-            //    .Select(s => s.LocationGeog.AsText())
-            //    .ToList();
-
-            var ss = await spuriousRepository.GetStoresBySubdivisionId(3514021);
-            var b = await spuriousRepository.GetBoundaryForSubdivision(3514021);
-            var top = await spuriousRepository.GetSubdivisionsForDensity(Core2.Stores.AlcoholType.All,
-                Core2.Subdivisions.EndOfDistribution.Top, 10);
-            var topBeer = await spuriousRepository.GetSubdivisionsForDensity(Core2.Stores.AlcoholType.Beer,
-                Core2.Subdivisions.EndOfDistribution.Top, 10);
-            var bottom = await spuriousRepository.GetSubdivisionsForDensity(Core2.Stores.AlcoholType.All,
-                Core2.Subdivisions.EndOfDistribution.Bottom, 10);
         }
     }
 }
