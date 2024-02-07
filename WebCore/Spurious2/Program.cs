23a8ad241d4ba81d5004d52e6e5e4c4eed7ba765
James McLachlan <JMcLachlan@carbonite.com>
2024-02-07 18:04:00 -0500

    Set CSP

--- WebCore/Spurious2/Program.cs
+++ WebCore/Spurious2/Program.cs
@@ -52,6 +52,80 @@ public class Program
 
         var app = builder.Build();
 
+        app.UseSecurityHeaders(o => o.AddContentSecurityPolicy(b =>
+        {
+            b.AddDefaultSrc().Self();
+            b.AddScriptSrc().WithNonce().StrictDynamic();
+            b.AddStyleSrc()
+                .Self()
+                .WithNonce()
+                //.WithHash256("aqNNdDLnnrDOnTNdkJpYlAxKVJtLt9CtFLklmInuUAE=") // display:none
+                .WithHash256("ATeSIcqM6UxTblzmyyICgZWZUpvkDFfJxeK3WjeUN90=")
+                .WithHash256("6IXr1202gJ13H3beeZ6W5EIY+3gdXFsD2u9avWEe/90=")
+                .WithHash256("5ad8McBu1lCYD+EOXcLRS1FeuI8rw9mYscXkLiGKyRc=")
+                .WithHash256("4frXBI2FwF2LM/qKBg0E03wsgxAwjM9XkjmuHhsolJU=")
+                //.WithHash256("KOn1ayjT/YFGzQrAGWjKJYhgg5mJTDMbsUv9P5jyiBI=")
+                //.WithHash256("e3eOpOzUImZzwHNXItARyghMSoToPE9xvAaAnp9I4jY=")
+                //.WithHash256("Cxx6bOC0Zxb3YVgRhsPywgmydIMfs1Ev+hfQe+W0yww=")
+                //.WithHash256("mwfCSNT2gG4EguqqR9xx/OsUjpqqxZKhCvVZvIZxvZM=")
+                //.WithHash256("6TlvdS/xVe6xq0dnsTUGBPfB583S+afLe59gwkcGO38=")
+                //.WithHash256("m8CW1jJIAJykofZ3oTkCM8UbceyIjLdGYTHwN8uUaD8=")
+                //.WithHash256("XUA1wQVvMfqOjYpetEE3e/aFfHYzb7GM3b5ruPIuaQQ=")
+                //.WithHash256("t1qXqTDrhjkLrSuTK6cfQXxZgFvv6YshzZyqbOEqUSQ=")
+                //.WithHash256("MOC36pv13im130jzJVJepGgY1LHrSvcKFSspd+WdfM0=")
+                //.WithHash256("1YWm9UYb0n5iwyXUot7Wd42C9HJbKoZL4U8V0/n/uYo=")
+                //.WithHash256("sYIZGfQC/euJyM8mmGfPuF6eb9eo/HeULI/gzbBfFoQ=")
+                //.WithHash256("Sqd2AvWzMkQ0Y3jTs3zazfKy/NYFiCX2yFrS2vEdOD4=")
+                .WithHash256("Ev6x8tBHdhQy7B3Y75SlaTnHyEhorCajhRI7BtClp8w=")
+                //.WithHash256("4VwUCoMkb7ArbBGLMdVijUl1S6eLrBEXcTKEwPYiY9I=")
+                //.WithHash256("LmDQlLw06KJf1PMXYZEHOXFHH5ZV2Vr59qG0oz6B444=")
+                //.WithHash256("Ncn4kC3pHMfsvdfwSz1RrfJ2Ow2T9Ao1mmpFTuHEAh4=")
+                //.WithHash256("rYyT5f4Pj/P23pPIA6YGWr/Oqz0Zt3DFihIX1DbOJ38=")
+                //.WithHash256("oBFTyWUR8pji6AIv/Z4Dxq5rvbCAf88+YvElwUlpT2o=")
+                //.WithHash256("sHOkVENLgOoxswROF9gq3L5SYgBiJwLsHfoCT1MllyA=")
+                //.WithHash256("cSCi0/BoKuxU5ey+XafI0ARvUYgp4joz2hD4uanrT78=")
+                //.WithHash256("x7qDFMGocoH4J5ikH3jAxpp+poqrmiD/n/EUzIziPnI=")
+                //.WithHash256("oB8CznOA36AjMxKbkJ2eahjLajSRpPRK1mgzv6/MCDo=")
+                //.WithHash256("lhbN2ffkCwt8GvAcigdJnZnV/LNMmLkBLnKczLRQWDI=")
+                //.WithHash256("iinr+Ixdnc78yCAXH0e7obpDu2RObuh4ZKW0jL8ziHM=")
+                //.WithHash256("Jf/QEz4Hou+P/x7xBRfgJ3cDcenvUb69eHpD7hgpDqQ=")
+                //.WithHash256("coh3armyBiterIe2Mh+pk8rXurGmMFp5Lcd/ptz54F0=")
+                .UnsafeHashes() // allow use of hashes on style elements, including the login error list
+                .StrictDynamic()
+                .WithHashTagHelper();
+            b.AddWorkerSrc().From("blob:");
+            b.AddConnectSrc().Self()
+                .From("canadacentral-1.in.applicationinsights.azure.com")
+                .From("atlas.microsoft.com")
+                .From("dc.services.visualstudio.com");
+            b.AddFontSrc().Self().From("atlas.microsoft.com");
+            b.AddFrameSrc().From("https://challenges.cloudflare.com");
+            b.AddImgSrc().Self().Blob().Data();
+        })
+.AddFrameOptionsDeny()
+.AddContentTypeOptionsNoSniff()
+.AddReferrerPolicyNoReferrer()
+.AddPermissionsPolicy(b =>
+{
+    b.AddMidi().None();
+    b.AddFullscreen().None();
+    b.AddAccelerometer().None();
+    b.AddAmbientLightSensor().None();
+    b.AddAutoplay().None();
+    b.AddCamera().None();
+    b.AddEncryptedMedia().None();
+    b.AddFederatedLearningOfCohortsCalculation().None();
+    b.AddGeolocation().None();
+    b.AddMagnetometer().None();
+    b.AddMicrophone().None();
+    b.AddPayment().None();
+    b.AddPictureInPicture().Self().For("https://challenges.cloudflare.com");
+    b.AddSpeaker().None();
+    b.AddSyncXHR().None();
+    b.AddUsb().None();
+    b.AddVR().None();
+}));
+
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
