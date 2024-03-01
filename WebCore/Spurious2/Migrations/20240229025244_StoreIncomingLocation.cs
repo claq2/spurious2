77245c1a40d47d55283589dc95cf879b630f527a
James McLachlan <jmclachlan@opentext.com>
2024-03-01 11:21:37 -0500

    Updates, clean up migration

--- WebCore/Spurious2/Migrations/20240229025244_StoreIncomingLocation.cs
+++ WebCore/Spurious2/Migrations/20240229025244_StoreIncomingLocation.cs
@@ -1,29 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 using NetTopologySuite.Geometries;
 
 #nullable disable
 
-namespace Spurious2.Migrations
+namespace Spurious2.Migrations;
+
+/// <inheritdoc />
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0022:Use expression body for method", Justification = "<Pending>")]
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>")]
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>")]
+public partial class StoreIncomingLocation : Migration
 {
     /// <inheritdoc />
-    public partial class StoreIncomingLocation : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<Point>(
-                name: "Location",
-                table: "StoreIncoming",
-                type: "geography",
-                nullable: false);
-        }
+        migrationBuilder.AddColumn<Point>(
+            name: "Location",
+            table: "StoreIncoming",
+            type: "geography",
+            nullable: false);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "Location",
-                table: "StoreIncoming");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Location",
+            table: "StoreIncoming");
     }
 }
