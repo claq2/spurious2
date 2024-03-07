af90644c39a35aaf6c714481aa10ea5ef85c2ba9
James McLachlan <claquer@gmail.com>
2024-03-06 22:52:17 -0500

    Clean up

--- Spurious2.Function2/Function1.cs
+++ Spurious2.Function2/Function1.cs
@@ -3,15 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace Spurious2.Function2
+namespace Spurious2.Function2;
+
+public class Function1(ILogger<Function1> logger, IGreeterService greeterService)
 {
-    public class Function1(ILogger<Function1> logger, IGreeterService greeterService)
+    [Function("Function1")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
-        [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
-        {
-            logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult(greeterService.GetGreeting("James"));
-        }
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult(greeterService.GetGreeting("James"));
     }
 }
