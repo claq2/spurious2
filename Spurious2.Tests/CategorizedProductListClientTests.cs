9caccc2d0abc20f61977c9ea5956e9f43105ad67
James McLachlan <claquer@gmail.com>
2024-03-11 12:22:14 -0400

    Page 2 test

--- Spurious2.Tests/CategorizedProductListClientTests.cs
+++ Spurious2.Tests/CategorizedProductListClientTests.cs
@@ -24,6 +24,23 @@ public class CategorizedProductListClientTests
         productList.Should().OnlyContain(p => p.PackageVolume > 0);
         productList.Should().OnlyContain(p => p.ProductPageUrl.Scheme == "https" && p.ProductPageUrl.DnsSafeHost == "www.lcbo.com");
         productList.Should().OnlyContain(p => p.Size == "750");
+        var pageOneIds = productList.Select(p => p.Id);
+
+        // Get page 2
+        prods = await client.GetProductList(9, Core.LcboImporting.Domain.ProductType.Wine, Core.LcboImporting.Domain.ProductSubtype.Red)
+            .ConfigAwait();
+        prods.results.Length.Should().Be(9);
+        productList = prods.results.GetProducts(Core.LcboImporting.Domain.ProductType.Wine);
+        productList.Count.Should().Be(9);
+        productList.Should().OnlyContain(p => p.Id > 0);
+        productList.Should().OnlyContain(p => p.LiquorType == "Wine");
+        productList.Should().OnlyContain(p => !string.IsNullOrWhiteSpace(p.Name));
+        productList.Should().OnlyContain(p => p.PackageVolume > 0);
+        productList.Should().OnlyContain(p => p.ProductPageUrl.Scheme == "https" && p.ProductPageUrl.DnsSafeHost == "www.lcbo.com");
+        productList.Should().OnlyContain(p => p.Size == "750");
+        var pageTwoIds = productList.Select(p => p.Id);
+        var overlappingIds = pageOneIds.Intersect(pageTwoIds).ToList();
+        overlappingIds.Count.Should().Be(0);
     }
 
     private static CategorizedProductListClient CreateCategorizedProductListClient()
