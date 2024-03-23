003dea98d996f3b0fdb209b0b5d13c311f99b247
James McLachlan <claquer@gmail.com>
2024-03-23 01:53:41 -0400

    All fast logging

--- WebCore/Spurious2.Core2/GeneratedLog.cs
+++ WebCore/Spurious2.Core2/GeneratedLog.cs
@@ -48,4 +48,34 @@ public static partial class GeneratedLog
 
     [LoggerMessage(EventId = 14, Level = LogLevel.Error, Message = "Failed to create one of the containers after 3 tries")]
     public static partial void FailedToCreateContainer(this ILogger logger);
+
+    [LoggerMessage(EventId = 15, Level = LogLevel.Information, Message = "Cleared for importing")]
+    public static partial void ClearedForImporting(this ILogger logger);
+
+    [LoggerMessage(EventId = 16, Level = LogLevel.Information, Message = "Last product done")]
+    public static partial void SignalLastProductDone(this ILogger logger);
+
+    [LoggerMessage(EventId = 17, Level = LogLevel.Information, Message = "Processed product {ProductId}")]
+    public static partial void ProcessedProduct(this ILogger logger, string productId);
+
+    [LoggerMessage(EventId = 18, Level = LogLevel.Information, Message = "Found {Count} inventory items for product {ProductId}")]
+    public static partial void FoundInventoryForProduct(this ILogger logger, int count, string productId);
+
+    [LoggerMessage(EventId = 19, Level = LogLevel.Information, Message = "Processed inventory {ProductId}")]
+    public static partial void ProcessedInventory(this ILogger logger, string productId);
+
+    [LoggerMessage(EventId = 20, Level = LogLevel.Information, Message = "Processed store {StoreId}")]
+    public static partial void ProcessedStore(this ILogger logger, string storeId);
+
+    [LoggerMessage(EventId = 21, Level = LogLevel.Information, Message = "Processed last product {Contents}")]
+    public static partial void ProcessedLastProduct(this ILogger logger, string contents);
+
+    [LoggerMessage(EventId = 22, Level = LogLevel.Information, Message = "Processed last inventory {Contents}")]
+    public static partial void ProcessedLastInventory(this ILogger logger, string contents);
+
+    [LoggerMessage(EventId = 23, Level = LogLevel.Information, Message = "Ended importing, doing DB update (no, not really :)")]
+    public static partial void EndedImporting(this ILogger logger);
+
+    [LoggerMessage(EventId = 24, Level = LogLevel.Information, Message = "Ended DB update")]
+    public static partial void EndedDbUpdate(this ILogger logger);
 }
