6dc7e76b2e75b16556efd3a8dde53e814efb4c7a
James McLachlan <claquer@gmail.com>
2024-04-01 23:04:01 -0400

    List

--- WebCore/Spurious2/spurious2/src/App.tsx
+++ WebCore/Spurious2/spurious2/src/App.tsx
@@ -5,13 +5,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import DefaultMap from "./components/DefaultMap";
 import Container from "@mui/material/Container";
+import SubdivisionList from "./components/SubdivisionList";
 
 const App = () => {
   return (
     <BrowserRouter>
       <NavBar />
       <Container>
+        <div>
+          <h1>Alcohol Density per Census Subdivision</h1>
+        </div>
         <DefaultMap />
+        <SubdivisionList />
       </Container>
       <Routes>
         <Route path="/" element={<Home />} />
