47b28606015b658493c3456dba61b90d4f57783f
James McLachlan <claquer@gmail.com>
2024-04-02 21:55:40 -0400

    Showing list better

--- WebCore/Spurious2/spurious2/src/components/SubdivisionList.tsx
+++ WebCore/Spurious2/spurious2/src/components/SubdivisionList.tsx
@@ -31,26 +31,22 @@ const SubdivisionList = () => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell></TableCell>
+            <TableCell>Subdivision&nbsp;Name</TableCell>
+            <TableCell align="right">Population</TableCell>
+            <TableCell align="right">Density&nbsp;(L/person)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {rows.map((row, index) => (
             <TableRow
               key={row.name}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell scope="row">{index + 1}.</TableCell>
+              <TableCell>{row.name}</TableCell>
               <TableCell align="right">{row.fat}</TableCell>
               <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
