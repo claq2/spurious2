de0d210ffce5a1a4947ee9e90e04962589e9d770
James McLachlan <claquer@gmail.com>
2024-05-04 10:29:12 -0400

    Trying to show point

--- WebCore/Spurious2/spurious2/src/components/MapController.tsx
+++ WebCore/Spurious2/spurious2/src/components/MapController.tsx
@@ -1,10 +1,17 @@
 import React, { useContext, useEffect } from "react";
 import { AzureMapsContext, IAzureMapsContextProps } from "react-azure-maps";
-import { data, layer, source } from "azure-maps-control";
+import {
+  MapMouseEvent,
+  PopupOptions,
+  data,
+  layer,
+  source,
+  Shape,
+} from "azure-maps-control";
 import MapComponent from "./MapComponent";
 import { useLazyGetStoresBySubdivisionIdQuery } from "../services/stores";
 import { useLazyGetBoundaryBySubdivisionIdQuery } from "../services/subdivisions";
-import { Store } from "../services/types";
+import { Inventory, Store } from "../services/types";
 
 const dataSourceRef = new source.DataSource();
 const lineLayer = new layer.LineLayer(dataSourceRef, undefined, {
@@ -19,10 +26,14 @@ const polygonLayer = new layer.PolygonLayer(dataSourceRef, undefined, {
     ["==", ["geometry-type"], "MultiPolygon"],
   ], //Only render Polygon or MultiPolygon in this layer.
 });
-const layerRef = new layer.SymbolLayer(dataSourceRef, "symbol", {
+const pointLayer = new layer.SymbolLayer(dataSourceRef, "symbol", {
   filter: ["==", ["geometry-type"], "Point"],
   iconOptions: { allowOverlap: true, image: "pin-blue" },
 });
+
+const alcoholTemplate = "<div>{alcoholType}: {volume} L</div>";
+const popupTemplate =
+  '<div class="customInfobox"><div class="name">{name}</div><div class="name">{city}</div>{alcoholTypes}</div>';
 
 interface MapControllerProps {
   subdivisionId: number | undefined;
@@ -43,7 +54,59 @@ const MapController = ({ subdivisionId }: MapControllerProps) => {
       mapRef.sources.add(dataSourceRef);
       mapRef.layers.add(lineLayer);
       mapRef.layers.add(polygonLayer);
-      mapRef.layers.add(layerRef);
+      mapRef.layers.add(pointLayer);
+
+      const popup: PopupOptions = {
+        pixelOffset: [0, -18],
+        //closeButton: false
+      };
+
+      mapRef.events.add("click", pointLayer, (e: MapMouseEvent) => {
+        //console.log('click');
+        //Make sure that the point exists.
+        if (e.shapes && e.shapes.length > 0) {
+          //console.log(e.shapes[0]);
+          //const x: data.Feature<data.Geometry, any> = e.shapes[0] as data.Feature<data.Geometry, any>;
+          const s = e.shapes[0] as Shape;
+          //x.properties;
+          // console.log(x);
+          // console.log(s);
+          //console.log('x.prop');
+          //console.log(x.properties['name']);
+          const storeName: string = s.getProperties()["name"] as string;
+          const cityName: string = s.getProperties()["city"] as string;
+          const inventories: Inventory[] = s.getProperties()[
+            "inventories"
+          ] as Inventory[];
+          let inventoryDiv = "";
+          inventories.forEach((i) => {
+            if (i.alcoholType && i.volume)
+              return (inventoryDiv += alcoholTemplate
+                .replace(/{alcoholType}/g, i.alcoholType)
+                .replace(/{volume}/g, i.volume.toString()));
+          });
+          const content: string = popupTemplate
+            .replace(/{name}/g, storeName)
+            .replace(/{city}/g, cityName)
+            .replace(/{alcoholTypes}/g, inventoryDiv);
+          //console.log('s.getProp');
+          //console.log(s.getProperties()['name']);
+          //console.log(s.getProperties()['inventories']);
+          //let properties = e.shapes[0].properties;// getProperties();
+          //content = popupTemplate.replace(/{name}/g, properties.name).replace(/{description}/g, properties.description);
+          const coordinate = s.getCoordinates();
+
+          popup.setOptions({
+            //Update the content of the popup.
+            content: content,
+
+            //Update the popup's position with the symbol's coordinate.
+            position: coordinate as data.Position,
+          });
+          //Open the popup.
+          popup.open(mapRef);
+        }
+      });
     }
   }, [isMapReady, mapRef]);
 
