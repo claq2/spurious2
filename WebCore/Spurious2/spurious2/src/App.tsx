39a6693d48319b8ed784d78fd73f5d51c7e41896
James McLachlan <claquer@gmail.com>
2024-05-05 12:25:43 -0400

    Clean up

--- WebCore/Spurious2/spurious2/src/App.tsx
+++ WebCore/Spurious2/spurious2/src/App.tsx
@@ -1,23 +1,7 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
-import {
-  // BrowserRouter,
-  // Routes,
-  Route,
-  RouterProvider,
-  // Outlet,
-} from "react-router-dom";
-// import NavBar, { dataLoader } from "./components/NavBar";
-// import DefaultMap from "./components/DefaultMap";
-// import Container from "@mui/material/Container";
-// import SubdivisionList from "./components/SubdivisionList";
-// import { useGetDensitiesQuery } from "./services/densities";
-// import { Density, Subdivision } from "./services/types";
-// import {
-//   useLazyGetSubdivisionsByDensityQuery,
-//   useLazyGetBoundaryBySubdivisionIdQuery,
-// } from "./services/subdivisions";
+import { Route, RouterProvider } from "react-router-dom";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -48,17 +32,6 @@ const App = () => {
   return (
     <>
       <RouterProvider router={router} />
-      {/* <Container>
-        <div>
-          <h1>Alcohol Density per Census Subdivision</h1>
-        </div>
-        <DefaultMap />
-        <SubdivisionList />
-      </Container> */}
-      {/* <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:id" element={<Home />} />
-        </Routes> */}
     </>
   );
 };
