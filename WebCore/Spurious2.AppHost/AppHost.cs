1fc97d75f5ef14c0c8a7a3c6240be5c89d96f564
James McLachlan <claquer@gmail.com>
2025-11-13 20:37:58 -0500

    Updates, run storage and function

--- WebCore/Spurious2.AppHost/AppHost.cs
+++ WebCore/Spurious2.AppHost/AppHost.cs
@@ -1,3 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var db = builder.AddConnectionString("spuriousdb");
+
+var storage = builder.AddAzureStorage("storage")
+                     .RunAsEmulator(az => az.WithLifetime(ContainerLifetime.Persistent))
+                     ;
+
+var blobs = storage.AddBlobs("blobs");
+
+var functions = builder.AddAzureFunctionsProject<Projects.Spurious2_Function2>("functions")
+                       .WithHostStorage(storage)
+                       .WithReference(db)
+                       .WithReference(blobs)
+                       .WaitFor(db)
+                       .WaitFor(storage)
+                       .WaitFor(blobs);
+
 builder.Build().Run();
