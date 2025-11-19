82e11ab4c02b9110c569da3ee7f073ba1d3379af
James McLachlan <claquer@gmail.com>
2025-11-19 16:50:50 -0500

    Back to normal cctor

--- Functions/Spurious2.QueueProcessors.Products/ProductsProcessor.cs
+++ Functions/Spurious2.QueueProcessors.Products/ProductsProcessor.cs
@@ -1,17 +1,25 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Spurious2.Core2;
 using Spurious2.Core2.Lcbo;
 
 namespace Spurious2.QueueProcessors.Products;
 
-public class ProductsProcessor(ILogger<ProductsProcessor> logger, IImportingService importingService)
+public class ProductsProcessor
 {
+    private readonly ILogger<ProductsProcessor> _logger;
+    private readonly IImportingService _importingService;
+
+    public ProductsProcessor(ILogger<ProductsProcessor> logger, IImportingService importingService)
+    {
+        _logger = logger;
+        _importingService = importingService;
+    }
+
     [Function(nameof(ProductsProcessor))]
     public async Task Run([QueueTrigger("products", Connection = "queues")] string productId)
     {
-        logger.LogInformation("Product queue trigger called for {ProductId}", productId);
-        await importingService.ProcessProductBlob(productId).ConfigAwait();
-        logger.LogInformation("C# queue trigger function processed product blob ProductId: {ProductId}", productId);
+        _logger.LogInformation("Product queue trigger called for {ProductId}", productId);
+        await _importingService.ProcessProductBlob(productId).ConfigureAwait(false);
+        _logger.LogInformation("C# queue trigger function processed product blob ProductId: {ProductId}", productId);
     }
 }
