7f39e54cd5d50da208797aceac7544b7097e06ee
James McLachlan <claquer@gmail.com>
2025-11-22 14:59:58 -0500

    Reading products

--- Functions/Spurious2.Processors.Products/Functions.cs
+++ Functions/Spurious2.Processors.Products/Functions.cs
@@ -1,13 +1,22 @@
+using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Spurious2.Core2.Lcbo;
 
 namespace Spurious2.Processors.Products;
 
-public static class Functions
+public class Functions(IImportingService importingService)
 {
-    public static void ProcessQueueMessage([QueueTrigger("products", Connection = "queues")] string message, 
+    public async Task ProcessQueueMessage([QueueTrigger("products", Connection = "queues")] string productId,
+        [Blob("inventories", FileAccess.Write, Connection = "blobs")] BlobContainerClient inventoriesClient,
+        [Queue("inventories", Connection = "queues")] QueueClient inventoriesQueue,
         ILogger logger)
     {
-        logger.LogInformation(message);
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(productId);
+        ArgumentNullException.ThrowIfNull(inventoriesClient);
+        ArgumentNullException.ThrowIfNull(importingService);
+        await importingService.ProcessProductBlob(inventoriesClient, inventoriesQueue, productId).ConfigureAwait(false);
+        logger.LogInformation(productId);
     }
 }
