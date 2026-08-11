ee03e24635b0c3c853153ab680ce211f15e2e3f0
James McLachlan <claquer@gmail.com>
2026-08-11 19:47:27 -0400

    Add SQL services

--- Workers/Scraper/Program.cs
+++ Workers/Scraper/Program.cs
@@ -1,6 +1,10 @@
+using Ardalis.Specification;
 using Azure.Storage.Blobs;
 using Azure.Storage.Queues;
+using Microsoft.EntityFrameworkCore;
 using Scraper;
+using Spurious2.Core2;
+using Spurious2.Infrastructure;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -53,6 +57,18 @@ builder.Services.AddKeyedSingleton("inventoryblobsclient", (sp, s) =>
     var blobContainerClient = blobServiceClient.GetBlobContainerClient("inventory");
     return blobContainerClient;
 });
+
+builder.Services.AddDbContextFactory<SpuriousContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("spuriousdb"),
+        b => b.UseNetTopologySuite()
+            .EnableRetryOnFailure()
+            .MigrationsAssembly("Spurious2"))
+);
+builder.EnrichSqlServerDbContext<SpuriousContext>();
+
+builder.Services.AddScoped<ISpuriousRepository, SpuriousRepository>();
+builder.Services.AddScoped<ISpuriousService, SpuriousService>();
+builder.Services.AddScoped(typeof(IReadRepositoryBase<>), typeof(SpuriousSpecRepository<>));
+builder.Services.AddScoped(typeof(IRepositoryBase<>), typeof(SpuriousSpecRepository<>));
 
 var host = builder.Build();
 host.Run();
