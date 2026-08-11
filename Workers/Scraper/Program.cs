8185b88b0853aaf67714940f18e3ac8d5d550e06
James McLachlan <claquer@gmail.com>
2026-08-11 13:34:52 -0400

    Adding services

--- Workers/Scraper/Program.cs
+++ Workers/Scraper/Program.cs
@@ -3,7 +3,10 @@ using Scraper;
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddServiceDefaults();
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddHostedService<ScraperWorker>();
+builder.Services.AddHostedService<ProductsWorker>();
+builder.Services.AddHostedService<InventoriesWorker>();
+builder.Services.AddHostedService<StoresWorker>();
 
 var host = builder.Build();
 host.Run();
