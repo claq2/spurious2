6e313e520f9be307063ce5d4e6470c68bfc27522
James McLachlan <claquer@gmail.com>
2026-08-15 21:08:58 -0400

    Clean up

--- Workers/Scraper/Program.cs
+++ Workers/Scraper/Program.cs
@@ -16,9 +16,7 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddServiceDefaults();
 builder.Services.AddHostedService<ScraperWorker>();
-builder.Services.AddHostedService<InventoriesWorker>();
 builder.Services.AddHostedService<StoresWorker>();
-// Configure queue clients to use base64 encoding for message content
 
 builder.AddAzureQueueServiceClient(connectionName: "queues", configureClientBuilder: clientBuilder =>
         clientBuilder.ConfigureOptions(options =>
